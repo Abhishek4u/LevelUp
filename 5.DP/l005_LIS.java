deb8d58b9e9c813239f4255885e8464534f0fcbb
Abhishek <abhishekbhatia4u@gmail.com>
2020-10-06 03:10:43 +0530

    Partially Added Codes(Oct-5)

--- 5.DP/l005_LIS.java
+++ 5.DP/l005_LIS.java
@@ -8,6 +8,8 @@ public class l005_LIS {
         LIS_set();
     }
 
+    // --------------------------OCT-3-CODES(LIS class)---------------------------- //
+
     // Leetcode 300. Longest Increasing Subsequence
     // https://leetcode.com/problems/longest-increasing-subsequence/
 
@@ -267,7 +269,199 @@ public class l005_LIS {
         return maxLen;
     }
 
+	//  Largest square formed in a matrix 
+    // https://practice.geeksforgeeks.org/problems/largest-square-formed-in-a-matrix/0
 
+	public static int maxSquare(int arr[][], int i, int j,int dp[][]) {
+
+		if(dp[i][j] != 0) return dp[i][j];
+	    
+	    int minSize = (int) 1e9;
+	    
+	    int dir[][] = {{1,0}, {0,1}, {1,1}};
+	    
+	    for(int d[] : dir) {
+	        int x = i + d[0];
+	        int y = j + d[1];
+	        
+	        if(x >= 0 && y >= 0 && x < arr.length && y < arr[0].length) {
+	            
+	            if(arr[x][y] == 0) {
+	                minSize = 0;
+	                break;
+	                
+	            } else {
+	                minSize = Math.min(minSize, maxSquare(arr,x,y,dp));
+	            }
+	        } else {
+                minSize = 0; 
+                // it can't be expanded bcz all elts are not present for making square
+            }
+
+	    }
+	    
+	    return dp[i][j] =  minSize == 1e9 ? 1 : minSize + 1;
+	}
+
+    public static int findMax(int arr[][]) {
+	    int n = arr.length,m = arr[0].length;
+	    
+	    int max = 0;
+		int dp[][] = new int[n][m];
+	    
+	    for(int i = 0; i < n;i++) {
+	        for(int j = 0; j < m;j++) {
+	            
+	            if(arr[i][j] == 1) {
+	                max = Math.max(maxSquare(arr,i,j,dp), max);
+	            }
+	        }
+	    }
+	    return max;
+	}
+
+    // Leetcode 413. Arithmetic Slices
+    // https://leetcode.com/problems/arithmetic-slices/
+
+    public int numberOfArithmeticSlices(int[] A) {
+        int n = A.length;
+        if(n < 3) return 0;
+        
+        int ans = 0;
+        int count = 0;
+        
+        for(int i = 1; i < n - 1; i++) {
+            
+            int d1 = A[i] - A[i-1];
+            int d2 = A[i+1] - A[i];
+            
+            if(d1 == d2) ans += (++count);
+            else count = 0;
+        }
+        return ans;
+    }
+
+    // --------------------------OCT-5-CODES(LIS class)---------------------------- //
     
+    //------------------------------------TODO----------------------------------//
+
+
+    // Leetcode 1218. Longest Arithmetic Subsequence of Given Difference
+
+    public int longestSubsequence(int[] arr, int d) {
+        HashMap<Integer, Integer> hm = new HashMap<> ();
+        
+        int maxLen = 0;
+        for(int ele : arr) {
+            int prevElt = ele - d;
+            // previous elt of current ap( a0 + d = a1 => a0 = a1 - d)
+            
+            hm.put(ele, hm.getOrDefault(prevElt, 0) + 1);
+            maxLen = Math.max(maxLen, hm.get(ele));            
+        }
+        return maxLen;
+    }
+
+    // Leetcode 1027. Longest Arithmetic Subsequence
+
+    public int longestArithSeqLength(int[] A) {
+        int n = A.length;
+        HashMap<Integer,Integer>[] dp = new HashMap[n];
+        
+        int len = 0;
+        for(int i = 0; i < n;i++) dp[i] = new HashMap<Integer,Integer> ();
+        
+        for(int i = 0; i < n;i++) {
+            for(int j = i - 1; j >= 0; j--) {
+                int diff = A[i] - A[j];
+                
+                int currLen = dp[i].getOrDefault(diff,0);
+                int newLen = dp[j].getOrDefault(diff,1) + 1;
+                
+                dp[i].put(diff, Math.max(currLen, newLen));
+                
+                len = Math.max(len, dp[i].get(diff));
+            }
+        }
+        return len;
+    }
+
+    // Maximum sum increasing subsequence 
+    // https://practice.geeksforgeeks.org/problems/maximum-sum-increasing-subsequence4749/1
+
+    public int maxSumIS(int arr[], int n) {  
+        int[] dp = new int[n];
+        
+	    int maxSum = 0;
+	    for(int i = n-1;i >= 0;i--) {
+	       
+            int max = arr[i];
+            for(int j  = i+1; j < arr.length; j++) {
+                if(arr[j] > arr[i]) {
+                    max = Math.max(max, dp[j] + arr[i]);
+                }
+            }
+            dp[i] = max;
+	        maxSum = Math.max(maxSum, max);
+	    }
+	    
+	    return maxSum;
+	}
+
+    // Leetcode 446
+
+    //------------------------------------TODO----------------------------------//
+
+    // https://practice.geeksforgeeks.org/problems/longest-alternating-subsequence/0
+    // Longest alternating subsequence
+    
+    public static int maxAlternating(int arr[], int idx) {
+	    int n = arr.length;
+	    
+	    int dp[][] = new int[n][2] ; 
+	    // (0 -> decreasing slope, 1 -> increasing slope)
+	    
+	    int maxLen = 0;
+        
+	    for(int i = 0; i < n;i++) {
+	        
+	        dp[i][0] = dp[i][1] = 1;
+	        for(int j = i - 1; j >= 0; j--) {
+	            
+	            if(arr[i] > arr[j]) dp[i][1] = Math.max(dp[i][1], dp[j][0] + 1);
+	            if(arr[i] < arr[j]) dp[i][0] = Math.max(dp[i][0], dp[j][1] + 1);
+	        }
+	        
+	        maxLen = Math.max(maxLen, Math.max(dp[i][0], dp[i][1]) );
+	    }
+	    return maxLen;
+	}
+
+    public static int BuildingBridges(int[][] arr) {
+        int n = arr.length;
+
+        Arrays.sort(arr, (a,b) -> {
+            // if(a[0] == b[0] ) return b[1] - a[1];
+            // use above condition if you do not want to have
+            // 2 or more bridges from same point
+
+            return a[0] - b[0];
+        });
+
+        int dp[] = new int[n];
+        int len = 0;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i - 1; j >= 0;j--) {
+                if(arr[i][1] > arr[j][1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            len = Math.max(len, dp[i]);
+        }
+
+        return len;
+    }
+
 
 }
